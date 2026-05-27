9a44faaa036fd149e0ba506c302b8f6b0c86093b
Szymon Urban <szymonur88828@gmail.com>
2026-05-27 19:08:37 +0200

    add email service

--- routes/RestartPassword.ts
+++ routes/RestartPassword.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
+import crypto from "crypto";
 import { query } from "../db/db.js";
 import { validateEmail } from "../utils/vadidateEmail.js";
+import { sendResetPasswordEmail } from "../services/emailService.js";
 
 const router = express.Router();
 
@@ -20,13 +22,21 @@ router.post("/", async (req: Request, res: Response) => {
             "SELECT id_uzytkownika, login, email, imie, nazwisko FROM Uzytkownicy WHERE email = $1",
             [email],
         );
+        const resetToken = crypto.randomBytes(32).toString("hex");
 
         const user = result.rows[0];
         if (user) {
             console.log(`Wysyłam email z resetem do: ${email}`);
+
+            // Wysyłka prostego maila (możesz to później zamienić na link z tokenem)
+            sendResetPasswordEmail(email, resetToken).catch((err) => {
+                console.error("Failed to send email inside route:", err);
+                // Nie przerywamy odpowiedzi dla użytkownika, aby nie zdradzać czy email istnieje (Security best practice)
+            });
         } else {
             console.warn(`Próba resetu dla nieistniejącego emaila: ${email}`);
         }
+
         return res.status(200).json({
             message:
                 "Jeśli podany adres e-mail istnieje w naszej bazie, wysłaliśmy na niego link do resetu hasła.",
